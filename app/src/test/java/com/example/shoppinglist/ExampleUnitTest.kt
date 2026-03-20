61b8aacd29edcc674f24073e8a1a7f510481709e
dessyaid <malyuitenko@gmail.com>
2026-03-21 01:25:55 +0200

    test: ExampleUnitTest

--- app/src/test/java/com/example/shoppinglist/ExampleUnitTest.kt
+++ app/src/test/java/com/example/shoppinglist/ExampleUnitTest.kt
@@ -1,17 +1,74 @@
 package com.example.shoppinglist
 
-import org.junit.Test
-
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    private lateinit var db: ShoppingDatabase
+    private lateinit var dao: ShoppingDao
+
+    @Before
+    fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, ShoppingDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        dao = db.shoppingDao()
     }
+
+    @After
+    fun teardown() {
+        db.close()
+    }
+
+    @Test
+    fun insertItemTest() = runBlocking {
+        val item = ShoppingItem(name = "TestItem", isBought = false)
+        dao.insertItem(item)
+
+        val allItems = dao.getAllItems()
+        assertTrue(allItems.any { it.name == "TestItem" })
+    }
+
+    @Test
+    fun updateItemTest() = runBlocking {
+        val item = ShoppingItem(name = "TestItem", isBought = false)
+
+        dao.insertItem(item)
+
+        val itemFromDb = dao.getAllItems().find { it.name == "TestItem" }
+        assertNotNull(itemFromDb)
+
+        val newName = "NewTestItem"
+        dao.updateItem(itemFromDb!!.copy(name = newName))
+
+        val allItems = dao.getAllItems()
+        assertTrue(allItems.any { it.name == newName })
+
+    }
+
+    @Test
+    fun deleteItemTest() = runBlocking {
+        val item = ShoppingItem(name = "TestItem", isBought = false)
+        dao.insertItem(item)
+
+        val itemFromDb = dao.getAllItems().find { it.name == "TestItem" }
+        assertNotNull(itemFromDb)
+
+        dao.deleteItem(itemFromDb!!)
+
+        val allItems = dao.getAllItems()
+        assertFalse(allItems.any { it.name == "TestItem" })
+    }
+
+
 }
